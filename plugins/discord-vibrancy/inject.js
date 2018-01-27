007872efcc6632e39bd6d282c460492481ef5ec8
Khinenw <deu07115@gmail.com>
2018-01-27 15:40:41 +0900

    Oops

--- plugins/discord-vibrancy/inject.js
+++ plugins/discord-vibrancy/inject.js
@@ -15,20 +15,42 @@ module.paths.push(path.resolve(
 ));
 
 const vibrancy = module.require('electron-vibrancy');
+let counter = 0;
 
 console.log("vibrancy loaded");
-const electronProxy = new Proxy(electronOriginal.exports.BrowserWindow, {
-	construct(target, ...args) {
+
+const electronProxy = new Proxy(electronOriginal.exports, {
+	get(target, prop, receiver) {
+		if(prop === 'BrowserWindow' && counter === 0) {
+			counter++;
+			return browserWindowProxy;
+		} else {
+			return Reflect.get(...arguments);
+		}
+	}
+});
+
+const browserWindowProxy = new Proxy(electronOriginal.exports.BrowserWindow, {
+	construct(target, args) {
 		args[0].transparent = true;
+		if(args[0].backgroundColor) {
+			delete args[0].backgroundColor;
+		}
+		
 		console.log("Proxy active!");
 		const browserWindow = new target(...args);
 		vibrancy.SetVibrancy(browserWindow, 2);
 		
 		return browserWindow;
+	},
+	
+	get(target, prop) {
+		console.log("Get Prop", prop);
+		return Reflect.get(...arguments);
 	}
 });
 
-electronOriginal.exports.BrowserWindow = electronProxy;
+electronOriginal.exports = electronProxy;
 require.cache[electronCache] = electronOriginal;
 
 console.log("done cache monkeypatching");
